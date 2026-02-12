a5af17247890614cdfc829152b454ab0f0e6669e
Vinicius Tarouquela <viniciustmoura13@gmail.com>
2026-02-12 12:38:23 -0300

    adicionando index e tela home

---
+++ script.js
@@ -0,0 +1,32 @@
+function realizarLogin() {
+	var email = document.getElementById("email").value;
+	var senha = document.getElementById("senha").value;
+
+	if (!email) {
+		document.getElementById("email").style.borderColor = "tomato";
+		return;
+	}
+
+	if (!senha) {
+		document.getElementById("email").style.borderColor = "tomato";
+		return;
+	}
+
+	if (email === "senai@gmail.com" && senha === "senai") {
+		window.location.href = "home.html";
+	} else {
+		/*document.getElementById("senha_invalida").innerText =
+			"Login ou senha invalidos";*/
+		alert("Login ou senha invalidos");
+	}
+}
+
+function exibirSenha() {
+	var tipo_atual = document.getElementById("senha").type;
+
+	if (tipo_atual === "text") {
+		document.getElementById("senha").type = "password";
+	} else {
+		document.getElementById("senha").type = "text";
+	}
+}
